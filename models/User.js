8641fa4c6fc2ce52385ef3007c8a542f32fe85a1
ryanden2018 <denlinger.ryan@gmail.com>
2019-07-05 09:54:39 -0500

    heroku and local config

--- models/User.js
+++ models/User.js
@@ -1,7 +1,13 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL,
-  { dialect: 'postgres', protocol: 'postgres' });
+let sequelize;
+if(process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL,
+    { dialect: 'postgres', protocol: 'postgres' });
+} else {
+  sequelize = new Sequelize("postgres://postgres:abcdef@localhost:5432/roombuilder",
+    { dialect: 'postgres', protocol: 'postgres' });
+}
 
 class User extends Sequelize.Model { }
 User.init( {
