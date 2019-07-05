485da44c2181c565deda2844c43c91a4a114f83d
ryanden2018 <denlinger.ryan@gmail.com>
2019-07-05 10:27:02 -0500

    heroku and local config

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const uuid = require('uuid/v4');
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: ( process.env.DATABASE_URL ? ["https://roombuilder.herokuapp.com","http://roombuilder.herokuapp.com"] : "http://localhost:3000" ),
+  origin: ( process.env.DATABASE_URL ? ["https://movethatsofa.herokuapp.com","http://movethatsofa.herokuapp.com"] : "http://localhost:3000" ),
   methods: ['GET','POST','PATCH','DELETE','PUT','OPTIONS','HEAD'],
   allowedHeaders: 'Content-Type,Authorization,Content-Length,X-Requested-With,X-Prototype-Version,Origin,Allow,*',
   credentials: true,
